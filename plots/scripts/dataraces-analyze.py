3c590c190340c0ef78c8e5ba81bc6e231bac242b
Marek Chalupa <mchqwerty@gmail.com>
2023-01-18 10:59:21 +0100

    Fix plots

--- plots/scripts/dataraces-analyze.py
+++ plots/scripts/dataraces-analyze.py
@@ -79,7 +79,6 @@ data2["tsan-mem"] = data2["tsan-mem"].astype(float) / 1024.0
 data2 = data2[data2["hel-races"] != "TO"]
 data2["hel-mem"] = data2["hel-mem"].astype(float) / 1024.0
 data2 = data2[data2["vamos-races"] != "TO"]
-data2 = data2[data2["vamos-races"] != "None"]
 data2["vamos-mem"] = data2["vamos-mem"].astype(float) / 1024.0
 
 fig = plt.figure(figsize=(6,6))
@@ -99,69 +98,75 @@ plt.xlabel("Tool 2 memory [MB]")
 plt.title("Comparing memory consumption of Vamos vs TSan and Helgrind")
 plt.savefig("dataraces_memory.pdf", bbox_inches='tight', dpi=300) 
 
+data2["tsan-races"] = data2["tsan-races"].astype(int)
+data2["hel-races"] = data2["hel-races"].astype(int)
+data2["vamos-races"] = data2["vamos-races"].astype(int)
 
-# # In[192]:
-# plt.clf()
-# plt.figure()
+plt.clf()
+plt.figure(figsize=(6, 6))
 
-# data["vamos-has-race"] = data["vamos-races"] > 0
-# data["tsan-has-race"] = data["tsan-races"] > 0
-# data["hel-has-race"] = data["hel-races"] > 0
+tot = float(len(data2))
+tp = len(data2.loc[(data2["vamos-races"] > 0)  & (data2["tsan-races"] > 0)])  / tot
+tn = len(data2.loc[(data2["vamos-races"] == 0) & (data2["tsan-races"] == 0)]) / tot
+fp = len(data2.loc[(data2["vamos-races"] > 0)  & (data2["tsan-races"] == 0)]) / tot
+fn = len(data2.loc[(data2["vamos-races"] == 0) & (data2["tsan-races"] > 0)])  / tot
+verdicts = [tp, tn, fp, fn]
+labels = ["true positive", "true negative", "false positive", "false negative"]
 
-# tot = len(data)
-# tp = len(data.loc[data["vamos-has-race"] & data["tsan-has-race"]]) / tot
-# tn = len(data.loc[(data["vamos-has-race"] == 0) & (data["tsan-has-race"] == False)]) / tot
-# fp = len(data.loc[data["vamos-has-race"] & data["tsan-has-race"] == False]) / tot
-# fn = len(data.loc[data["vamos-has-race"] == False & data["tsan-has-race"]]) / tot
-# verdicts = [tp, tn, fp, fn]
-# labels = ["true positive", "true negative", "false positive", "false negative"]
+explode = [0, 0, 0.0, 0.0]
 
-# explode = [0, 0, 0, 0]
-  
-# # plotting data on chart
-# pie = plt.pie(verdicts, labels=labels, 
-#               explode=explode,
-#               autopct='%.2f%%')
-# plt.title("The correctness of verdicts (taking TSan as the base line)")
+# plotting data on chart
+pie = plt.pie(verdicts, labels=labels,
+              explode=explode,
+              autopct='%.2f%%',
+              #textprops={'fontsize': 14}
+              labeldistance=1.05,
+              radius=1.1
+              )
+plt.title("The correctness of verdicts (taking TSan as the base line)")
+for lbl, val in zip(pie[1], pie[2]):
+    # delete 0 procents
+    if val.get_text() == "0.00%":
+        val.set_text("")
+        lbl.set_text("")
 
-
-# plt.subplots_adjust(wspace=0.03, hspace=0)  
-# plt.savefig("dataraces_correctenss.pdf", bbox_inches='tight', dpi=300)
-
-
-# data["tsan-races-diff"] = data["vamos-races"] - data["tsan-races"]
-# data["hel-races-diff"] = data["vamos-races"] - data["hel-races"]
-
-# plt.clf()
-# figs_size = (10,3)
-# font_scale = 1
-# sns.set(rc={'figure.figsize':figs_size}, font_scale=font_scale)
-# sns.set_style("whitegrid")
-
-# fig, ax = plt.subplots(1, 2, sharey=True,
-#                       gridspec_kw={'width_ratios': [1, 1]})
-
-# b1 = sns.boxplot(data=data["tsan-races-diff"], ax=ax[0],
-#                  whiskerprops=dict(linewidth=3),
-#                  flierprops=dict(marker='o', markersize=2),#    markerfacecolor='red'),
-#                  capprops=dict(linewidth=3),
-#                  medianprops=dict(linewidth=3))
-# b2 = sns.boxplot(data=data["hel-races-diff"], ax=ax[1],
-#                  whiskerprops=dict(linewidth=3),
-#                  flierprops=dict(marker='o', markersize=2),#    markerfacecolor='red'),
-#                  capprops=dict(linewidth=3),
-#                  medianprops=dict(linewidth=3))
-
-# b1.set_xlabel('Vamos vs TSan', labelpad=15)
-# b1.set_ylabel('Δ in detected races', labelpad=15)
-# b2.set_xlabel('Vamos vs Helgrind', labelpad=15)
-# b2.set_ylabel("")
-# plt.title("The difference in the number of found data races (bigger means more found)")
-# plt.subplots_adjust(wspace=0.03, hspace=0)  
-# plt.savefig("dataraces_errors.pdf", bbox_inches='tight', dpi=300)
+plt.subplots_adjust(wspace=0.03, hspace=0)  
+plt.savefig("dataraces_correctenss.pdf", bbox_inches='tight', dpi=300)
 
 
-# #dataG = data.groupby("benchmark")
-# #sns.violinplot(data=dataG.mean()["vamos-has-race"])
+data2["tsan-races-diff"] = data2["vamos-races"] - data2["tsan-races"]
+data2["hel-races-diff"] = data2["vamos-races"] - data2["hel-races"]
+
+plt.clf()
+figs_size = (10,3)
+font_scale = 1
+sns.set(rc={'figure.figsize':figs_size}, font_scale=font_scale)
+sns.set_style("whitegrid")
+
+fig, ax = plt.subplots(1, 2, sharey=True,
+                      gridspec_kw={'width_ratios': [1, 1]})
+
+b1 = sns.boxplot(data=data2["tsan-races-diff"], ax=ax[0],
+                 whiskerprops=dict(linewidth=3),
+                 flierprops=dict(marker='o', markersize=2),#    markerfacecolor='red'),
+                 capprops=dict(linewidth=3),
+                 medianprops=dict(linewidth=3))
+b2 = sns.boxplot(data=data2["hel-races-diff"], ax=ax[1],
+                 whiskerprops=dict(linewidth=3),
+                 flierprops=dict(marker='o', markersize=2),#    markerfacecolor='red'),
+                 capprops=dict(linewidth=3),
+                 medianprops=dict(linewidth=3))
+
+b1.set_xlabel('Vamos vs TSan', labelpad=15)
+b1.set_ylabel('Δ in detected races', labelpad=15)
+b2.set_xlabel('Vamos vs Helgrind', labelpad=15)
+b2.set_ylabel("")
+plt.title("The difference in the number of found data2 races (bigger means more found)")
+plt.subplots_adjust(wspace=0.03, hspace=0)  
+plt.savefig("dataraces_errors.pdf", bbox_inches='tight', dpi=300)
+
+
+#dataG = data.groupby("benchmark")
+#sns.violinplot(data=dataG.mean()["vamos-has-race"])
 
 
