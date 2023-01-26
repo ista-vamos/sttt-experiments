f33eb07ce4f60bf541406b0d913a9bf9ec0f5377
Marek Chalupa <mchqwerty@gmail.com>
2023-01-26 10:50:44 +0100

    Update build docker

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+set -e
+
 if [[ "$OSTYPE" == "linux"* ]]; then
-	DOCKER_BUILDKIT=1 docker build . -f Dockerfile.buildkit
+	DOCKER_BUILDKIT=1 docker build . -f Dockerfile.buildkit -t vamos:fase
 else
-	docker build .
+	docker build . -t vamos:fase
 fi
+
+echo "Run command:"
+echo "docker run -ti -v "$(pwd)/results":/opt/results  vamos:fase"
